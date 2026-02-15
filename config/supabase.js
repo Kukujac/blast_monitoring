df4aa00ad19094ee1830ca3539963a78337092ef
Kukujac <kukujac@gmail.com>
2026-02-15 06:47:52 +0000

    Create supabase.js
    added a connection file

---
+++ config/supabase.js
@@ -0,0 +1,5 @@
+// Supabase configuration for frontend
+const SUPABASE_CONFIG = {
+    url: 'https://aahrwtlmemaqacubievh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhaHJ3dGxtZW1hcWFjdWJpZXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3ODg5MTgsImV4cCI6MjA4NjM2NDkxOH0.sCQwcgEtEh4VMcIswpF2xA7bHfnc2OiB911YAwZuzvc'
+};
